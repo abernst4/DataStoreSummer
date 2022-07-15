2ba8bbafc1f8a8ef2885694d40dfb93f25dce606
JosephBorodach <95253429+JosephBorodach@users.noreply.github.com>
2022-07-15 15:26:53 -0400

    First descriptive commit of my life - Deleting hub-app / stage 2 code

--- app/src/main/java/com/geekcap/javaworld/jpa/model/User.java
+++ app/src/main/java/com/geekcap/javaworld/jpa/model/User.java
@@ -13,25 +13,12 @@ import java.util.HashSet;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javax.annotation.processing.Generated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
-
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.JoinTable;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
