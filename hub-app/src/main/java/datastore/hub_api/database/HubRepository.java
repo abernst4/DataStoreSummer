705803e653db0c0458f8db758a06cc2b6a79bdaf
avi bernstein <abernst4@mail.yu.edu>
2022-07-19 12:43:06 -0400

    changes in hub; trying to use shareLive

--- hub-app/src/main/java/datastore/hub_api/database/HubRepository.java
+++ hub-app/src/main/java/datastore/hub_api/database/HubRepository.java
@@ -6,29 +6,15 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.*;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
-@Entity
-public class HubRepository extends PanacheEntityBase {
-    @Id
-    public long groupId;
+import io.quarkus.hibernate.orm.panache.
 
-    public InetAddress ia;
-
-    public HubRepository(long GroupId, InetAddress ia) {
-        this.groupId = GroupId;
-        this.ia = ia;
-    }
-
-    public HubRepository(){}
-
+public class HubRepository extends PanacheEntityBase{
     public Map<Long, InetAddress> toMap() {
         Map<Long, InetAddress> IdMap = new HashMap<>();
         List<HubRepository> groupList = HubRepository.listAll();
-
         for (HubRepository gs : groupList) {
             IdMap.put(gs.groupId, gs.ia);
         }
-
         return IdMap;
     }
 }
