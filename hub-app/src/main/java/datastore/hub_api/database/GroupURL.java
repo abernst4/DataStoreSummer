2aede43abeaecdcd7ed6ce87668edcd4a2d08b3d
JosephBorodach <95253429+JosephBorodach@users.noreply.github.com>
2022-07-19 12:40:32 -0400

    Consolidated everything into HubRoute

--- hub-app/src/main/java/datastore/hub_api/database/GroupURL.java
+++
@@ -1,9 +0,0 @@
-package datastore.hub_api.database;
-import java.net.URI;
-import javax.persistence.Entity;
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
-
-@Entity
-public class GroupURL extends PanacheEntity{
-    public URI uri; 
-}
