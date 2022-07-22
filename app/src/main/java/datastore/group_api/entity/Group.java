3606ad1069374a6be78e19c1875437ac6d884fe1
avi bernstein <abernst4@mail.yu.edu>
2022-07-22 12:00:08 -0400

    changes after nullPointer found

--- app/src/main/java/datastore/group_api/entity/Group.java
+++ app/src/main/java/datastore/group_api/entity/Group.java
@@ -1,7 +1,10 @@
 package datastore.group_api.entity;
+import javax.persistence.CascadeType;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
@@ -17,6 +20,10 @@ public class Group extends PanacheEntity{
     @OneToMany(mappedBy = "group", fetch = FetchType.EAGER)
     public List<User> users;
     public URL url; 
+    //@ElementCollection()
+    //@OneToOne(mappedBy= "group", fetch=FetchType.EAGER)
+    //public Map<Long,URL> urlMap = new HashMap<>(); 
+
 //    public Map<Long, URL> map; 
 }
 
